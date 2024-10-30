2f9efacefc70a372791ae0b591e7d66a96599661
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-30 06:37:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text1", {
+    strings: [
+        "Wishing you and your family joy and prosperity! 🌟",
+        "May the Festival of Lights bring happiness to your home! 🎆",
+        "Happy Diwali from Dhiraj Singh! 🪔"
+    ],
+
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true
+});
+
